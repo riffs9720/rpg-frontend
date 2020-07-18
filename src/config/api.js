e890da6f929c96ffe2684b8600ce3ee640c2092d
Riffs9720 <rferlasilva@gmail.com>
2020-07-18 00:15:15 -0300

    Configurado para acesso atraves da API do RPG ospedada no Heroku

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-//   export const api = axios.create({
-//   baseURL: 'https://megami-no-rpg-backend.herokuapp.com'
-// }) 
-
   export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://megami-no-rpg-backend.herokuapp.com'
 }) 
+
+//   export const api = axios.create({
+//   baseURL: 'http://localhost:3333'
+// }) 
